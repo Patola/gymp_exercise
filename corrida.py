15af92aa14c371d47af66c6aa49a717bac3190cf
Cláudio 'Patola' Sampaio <patola@gmail.com>
2019-06-07 14:01:33 -0300

    Fixed variable names and added current hour

--- corrida.py
+++ corrida.py
@@ -2,55 +2,60 @@
 # -*- coding: utf-8-*-
 
 import sys
-from dateutil import parser
 from datetime import datetime, timedelta
 from collections import OrderedDict
 
 """ Log Parser for Kart races """
 
 class Pilot:
-    nowPrefix=datetime.now().strftime("%Y-%m-%d") # this prefix will be fixed since class is declared
-    _all_pilots={} # we need to track our instances. A data dictionary keyed by code + name is ideal for this
-                   # "_" prefix means it's a protected class variable.
-    _maximum_lap = 4 # race will finish when lap_number get to this number
+    nowPrefix=datetime.now().strftime("%Y-%m-%d")  # this prefix will be fixed since class is declared
+    _all_pilots = {}  # we need to track our instances. A data dictionary keyed by code + name is ideal for this
+
+    _maximum_lap = 4  # race will finish when lap_number get to this number
     _best_overall_lap = timedelta.max
-    _total_race_time = timedelta(0) # timedelta.min is negative, not gonna risk inconsistencies
+    _total_race_time = timedelta(0)  # timedelta.min is negative, not gonna risk inconsistencies
 
     def __init__(self, name, code):
-        self.name=name
+        self.name = name
         self.start_time = None
         self.end_time = None
         self.time_dif_first_place = None
         self.code=code
-        self.best_lap_time=timedelta.max
-        self.max_lap=0
-        self.latest_lap_time=timedelta(0)
-        self.position=0
-        self.total_time=timedelta(0)
-        self.average_speed=0.0
+        self.best_lap_time = timedelta.max
+        self.max_lap = 0
+        self.latest_lap_time = timedelta(0)
+        self.position = 0
+        self.total_time = timedelta(0)
+        self.average_speed = 0.0
         self.valid = False
-        self.Finished = True
-        Pilot._all_pilots[self.code + " - " + self.name]=self # so that we can find by code and name
-        return self
+        self.current_hour = None
+        self.finished = True
+        Pilot._all_pilots[self.code + " - " + self.name] = self  # so that we can find by code and name
+        return None
 
     def add_lap(self, lap_hour, lap_number, lap_duration, average_lap_speed):
-        if self.lap_number == lap_number - 1: # Consecutive laps are enforced
-            self.max_lap = lap_number
+#        print ("add_lap: " +str(self.max_lap) + " " + str(lap_hour) + " " + str(lap_number) + " " + str(lap_duration.total_seconds()) + " " + str(average_lap_speed))
+        if self.max_lap == int(lap_number) - 1: # Consecutive laps are enforced
+            self.max_lap = int(lap_number)
             if self.max_lap == 1: # First time? Let's initialize some values!
                 self.start_time = lap_hour - lap_duration
+                self.current_hour = lap_hour
                 self.best_lap_time = lap_duration
                 self.total_time = lap_duration
                 self.valid = True # we should do that only once per pilot
             elif self.max_lap == maximum_lap:
-                self.Finished = True
+                self.finished = True
                 self.end_time = lap_hour
             if self.best_lap_time > lap_duration:
                 self.best_lap_time = lap_duration
             # new average speed is going to be total_distance / total_time
             # total_distance: (average lap speed * lap duration) + (previous average speed * total_time so far)
-            total_distance = (float(average_lap_speed) * float(lap_duration)) + (float(self.average_speed) * float(self.total_time))
-            # the NEW total time 
-            self.total_time = float(self.total_time + lap_duration)
+            total_distance = (float(average_lap_speed) * \
+                              lap_duration.total_seconds()) + \
+                              (float(self.average_speed) * \
+                              self.total_time.total_seconds())
+            # the NEW total time
+            self.total_time = (self.total_time + lap_duration).total_seconds()
             # and now the average speed calculation
             self.average_speed = float(total_distance / self.total_time)
             return True
@@ -64,14 +69,14 @@ class Pilot:
     def update_finished(cls):
         # first, let's order them by total_time and substitute the dictionary!
         # python 3 guarantess order of inclusion of dicts with OrderedDict
-        if Pilot._all_pilots == {}:
+        if len(Pilot._all_pilots) == 0:
             print(f'No elements in list')
             return False
         Pilot._all_pilots=OrderedDict(sorted(Pilot._all_pilots.items(), key=lambda x: x[1].position))
         pos_tmp=1
         pilot = None
         firstpilot = None
-        for pilot in Pilot._all_pilots:
+        for pilot in Pilot._all_pilots.values():
             pilot.position=pos_tmp
             if pilot.position == 1:
                 firstpilot = pilot
@@ -79,8 +84,8 @@ class Pilot:
             if pilot.best_lap_time < Pilot._best_overall_lap:
                 Pilot._best_overall_lap = pilot.best_lap_time
             pilot.time_dif_first_place = pilot.total_time - firstpilot.total_time
-            pilot.Finished = True
-            pilot.end_time = lap_hour
+            pilot.finished = True
+            pilot.end_time = pilot.current_hour 
         Pilot._total_race_time = pilot.total_time # pilot is the last pilot from the loop
         return True
 
@@ -125,20 +130,20 @@ def parse_line(fline):
     >>> parse_line("")
     [None, '', '', '', 0, datetime.timedelta(0), 0.0]
     >>> parse_line("23:49:08.277      038 - F.MASSA                           1            1:02.852                        44,275")
-    [datetime.datetime(2019, 6, 6, 23, 49, 8, 277000), '038', 'F.MASSA', 1, datetime.timedelta(0, 62, 852000), 44.275] 
+    [datetime.datetime(2019, 6, 7, 23, 49, 8, 277000), '038', 'F.MASSA', 1, datetime.timedelta(0, 62, 852000), 44.275]
     """
     elements=fline.split()
 
     # Basic consistency check
     # second condition for len > 2 prevents an index error in the next one
     if len(elements) != 7 or (len(elements)>2 and elements[2]!="-"):
-        return [None,'','','',0,timedelta(0),0.0]
+        return [None,'','',0,timedelta(0),0.0]
     return [
-        datetime.strptime(nowPrefix+" " + elements[0],'%Y-%m-%d %H:%M:%S.%f'),
+        datetime.strptime(Pilot.nowPrefix+" " + elements[0],'%Y-%m-%d %H:%M:%S.%f'),
         elements[1],
         elements[3],
         int(elements[4]),
-        timedelta(minutes=int(elements[5].split(":")[0]), seconds=float(elements[5].split(":")[1])),
+        timedelta(minutes = int(elements[5].split(":")[0]), seconds = float(elements[5].split(":")[1])),
         float(elements[6].split(",")[0] + "." + elements[6].split(",")[1])
     ];
 """
@@ -151,7 +156,7 @@ def parse_line(fline):
     5 - average speed, float with commas
 """
 
-    
+
 def parse_date(datestring):
     return datetime.strptime(Pilot.nowPrefix+' '+datestring,"%Y-%m-%d %H:%M:%S.%f")
 
@@ -167,11 +172,14 @@ def kartparser():
     3
     """
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) > 2:
         print(f'Usage: python3 {sys.argv[0]} <logfile>')
         return 2
 
-    inputfile=sys.argv[1]
+    if len(sys.argv) == 3:
+        inputfile=sys.argv[1]
+    else:
+        inputfile="/dados/git/gymp_exercise/corrida.log"
 
     try:
         f = open(inputfile, 'r')
@@ -185,7 +193,7 @@ def kartparser():
 
     # main loop for gobbling up data and setting up pilots
     fline=""
-    while fline in f.readlines():
+    for fline in f.readlines():
         elements=parse_line(fline)
         if elements[0] is None:
             print(f'Invalid line: ' + fline)
@@ -196,17 +204,19 @@ def kartparser():
             pilot.add_lap(elements[0], elements[3],
                 elements[4], elements[5])
 
-    # Finished race. Let's update positions, best lap time, total race duration.
+    # finished race. Let's update positions, best lap time, total race duration.
     Pilot.update_finished()
 
     print(f'Hora de chegada\t\tCódigo\tNome\tVoltas\tMelhor Volta\tVelocidade Média Total\tTempo Total')
     for pilot in Pilot.all_pilots():
-        print(strftime(pilot.end_time,'%H:%M:%S.%f') + '\t\t' + pilot.code + '\t' + pilot.name + '\t' + pilot.max_lap + '\t' +
+        print(datetime.strftime(pilot.end_time,'%H:%M:%S.%f') + '\t\t' + pilot.code + '\t' + pilot.name + '\t' + pilot.max_lap + '\t' +
               pilot.best_lap_time + '\t' + pilot.average_speed + '\t' + pilot.total_time)
     print(f'\n\nStatistics:\n')
     print(f'Melhor tempo de volta: ' + str(Pilot.best_lap()))
     print(f'Tempo total da corrida: ' + str(Pilot.total_time()))
 
+    return 0
+
 
 if __name__ == "__main__":
-   sys.exit(kartparser())
+    sys.exit(kartparser())
